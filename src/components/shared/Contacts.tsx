0420535a6deca622b75cdb95c31ab72dae0ea0ec
k.dubov <example@example.com>
2024-01-11 06:58:56 +0300

    refact contacts

--- src/components/shared/Contacts.tsx
+++ src/components/shared/Contacts.tsx
@@ -1,17 +1,20 @@
+import { contacts } from '@/content'
 import React from 'react'
 
 const Contacts = () => {
     return (
         <div className='bg-primary text-foreground px-9 py-4'>
             <ul className='flex flex-wrap gap-6'>
-                <li>
-                    <p className='text-lg font-medium'>Email me at</p>
-                    <a href="mailto:contact@website.com">contact@website.com</a>
-                </li>
-                <li>
-                    <p className='text-lg font-medium'>Call us</p>
-                    <a href="tel:0927627728525">0927 6277 28525</a>
-                </li>
+                {
+                    contacts.map(({ title, href, label }) => {
+                        return (
+                            <li key={href}>
+                                <p className='text-lg font-medium'>{title}</p>
+                                <a href={href}>{label}</a>
+                            </li>
+                        )
+                    })
+                }
             </ul>
         </div>
     )
--- src/content/index.ts
+++ src/content/index.ts
@@ -19,4 +19,19 @@ export const navbar = [
         label: 'Вопросы',
         route: '/faq'
     },
+];
+
+export const contacts = [
+    {
+        name: 'email',
+        title: 'Email me at',
+        href: 'mailto:contact@website.com',
+        label: 'contact@website.com'
+    },
+    {
+        name: 'phone',
+        title: 'Call us',
+        href: 'tel:0927627728525',
+        label: '0927 6277 28525'
+    },
 ]
