574990bc0ea0f3fbd6d49692192bc04846a3bb0c
k.dubov <example@example.com>
2024-01-11 08:54:50 +0300

    add hero section content

--- src/components/shared/Hero/Hero.tsx
+++ src/components/shared/Hero/Hero.tsx
@@ -4,15 +4,20 @@ import { Button } from '@/components/ui/button'
 import { MoveRight } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
+import { site } from '@/content'
 
 const Hero = () => {
+    const title = site.home?.hero?.title;
+    const description = site.home?.hero?.description;
+    const image = site.home?.hero?.image;
+
     return (
         <section className={`${styles.section}`}>
             <div className={`container ${styles.container}`}>
                 <div className={`${styles.content}`}>
-                    <h1 className="heading-1">Building stellar websites for early startups</h1>
+                    <h1 className="heading-1 lg:tracking-[-0.03em]">{title}</h1>
                     <p className="paragraph text-foregroundSecondary/70">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.
+                        {description}
                     </p>
                     <div className={`${styles.footer}`}>
                         <Button asChild className='w-full sm:w-fit'>
@@ -22,10 +27,10 @@ const Hero = () => {
                     </div>
                 </div>
                 <Image
-                    src={`/images/hero.png`}
+                    src={image}
                     width={638}
                     height={360}
-                    alt='Building stellar websites for early startups'
+                    alt={title}
                     className={`${styles.image}`}
                 />
             </div>
