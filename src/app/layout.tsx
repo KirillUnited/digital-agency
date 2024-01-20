b5a7704b989fdcc6494a3c267582a107e834de65
k.dubov <example@example.com>
2024-01-20 12:59:29 +0300

    add header blur effect

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,9 +26,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${montserrat.variable} wrapper`}>
+      <body className={`${montserrat.variable} wrapper bg-background-secondary`}>
         <Header />
-        <main className='flex-1'>{children}</main>
+        <main className='flex-1 bg-background'>{children}</main>
         <Footer />
       </body>
     </html>
--- src/components/shared/Header/Header.tsx
+++ src/components/shared/Header/Header.tsx
@@ -5,10 +5,12 @@ import { Button } from '../../ui/button'
 import MobileMenu from './MobileMenu'
 import Logo from '../Logo'
 import HeaderDrawer from './HeaderDrawer'
+import { cn } from '@/lib/utils'
+import styles from './styles.module.css'
 
 const Header = () => {
     return (
-        <header className='sticky z-50 top-0 bg-background-secondary'>
+        <header className={cn('sticky z-50 top-0 left-0', styles.header)}>
             <div className="container">
                 <div className="flex items-center justify-between min-h-24 py-4">
                     <Logo />
@@ -16,7 +18,6 @@ const Header = () => {
                         <Navbar variant='primary' />
                         <Button size={'sm'}>Обратная связь</Button>
                     </nav>
-                    {/* <MobileMenu /> */}
                     <HeaderDrawer />
                 </div>
             </div>
