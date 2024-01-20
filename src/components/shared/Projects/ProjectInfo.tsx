e99aac3e3dff30685b078f1115a7ee4ef0fb20c2
k.dubov <example@example.com>
2024-01-20 21:38:53 +0300

    refact project info

--- src/components/shared/Projects/ProjectInfo.tsx
+++ src/components/shared/Projects/ProjectInfo.tsx
@@ -4,24 +4,22 @@ type ProjectInfoProps = {
     [propName: string]: string
 }
 export default function ProjectInfo({ client, service, deliverable }: ProjectInfoProps) {
-    if (client || service || deliverable) {
-        return (
-            <div className='border-b border-foreground/30 pb-4 lg:pb-8'>
-                <ul className='flex flex-wrap justify-between gap-3'>
-                    <li>
-                        <span className='text-foreground/80 font-medium'>Client</span>
-                        <h6 className='heading-6'>{client}</h6>
-                    </li>
-                    <li>
-                        <span className='text-foreground/80 font-medium'>Service</span>
-                        <h6 className='heading-6'>{service}</h6>
-                    </li>
-                    <li>
-                        <span className='text-foreground/80 font-medium'>Deliverable</span>
-                        <h6 className='heading-6'>{deliverable}</h6>
-                    </li>
-                </ul>
-            </div>
-        )
-    }
+    return (
+        <div className='border-b border-foreground/30 pb-4 lg:pb-8'>
+            <ul className='flex flex-wrap justify-between gap-3'>
+                <li>
+                    <span className='text-foreground/80 font-medium'>Client</span>
+                    {client && <h6 className='heading-6'>{client}</h6>}
+                </li>
+                <li>
+                    <span className='text-foreground/80 font-medium'>Service</span>
+                    {service && <h6 className='heading-6'>{service}</h6>}
+                </li>
+                <li>
+                    <span className='text-foreground/80 font-medium'>Deliverable</span>
+                    {deliverable && <h6 className='heading-6'>{deliverable}</h6>}
+                </li>
+            </ul>
+        </div>
+    )
 }
