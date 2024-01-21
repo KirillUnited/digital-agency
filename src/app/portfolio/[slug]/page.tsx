7d1982307194ee249f6ea57f2c1f42147aa55d06
k.dubov <example@example.com>
2024-01-21 17:16:30 +0300

    [project template] generate static params

--- src/app/portfolio/[slug]/page.tsx
+++ src/app/portfolio/[slug]/page.tsx
@@ -1,4 +1,4 @@
-import { getProject } from '@/lib/getProjects'
+import getProjects, { ProjectType, getProject } from '@/lib/getProjects'
 import React from 'react';
 import ReactMarkdown from 'react-markdown'
 import ProjectsFooter from '@/app/portfolio/components/ProjectsFooter';
@@ -9,21 +9,24 @@ type Props = {
     params: { slug: string }
 }
 
-export async function generateMetadata({ params }: Props) {
-    const project = await getProject(params.slug);
+// export async function generateMetadata({ params }: Props) {
+//     const project = await getProject(params.slug);
 
-    return {
-        title: project?.title,
-    }
-}
+//     return {
+//         title: project?.title,
+//     }
+// }
+
+export async function generateStaticParams() {
+    const posts = await getProjects();
+   
+    return posts.map((post) => ({
+      slug: post?.slug,
+    }))
+  }
 
 const ProjectTemplate = async ({ params }: Props) => {
-    let project;
-    try {
-        project = await getProject(params.slug);        
-    } catch (error) {
-        console.error(error)
-    }
+    const project = await getProject(params.slug);
 
     return (
         <>
