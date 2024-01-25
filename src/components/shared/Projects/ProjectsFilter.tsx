b8fda4340270102d67211178b388c2685440ca71
k.dubov <example@example.com>
2024-01-25 16:22:10 +0300

    update projects filter

--- src/components/shared/Projects/ProjectsFilter.tsx
+++ src/components/shared/Projects/ProjectsFilter.tsx
@@ -1,13 +1,18 @@
 import { pricing } from '@/content'
+import Link from 'next/link'
 import React from 'react'
 
 export default function ProjectsFilter() {
     return (
         <div className='flex flex-wrap items-center justify-center gap-4 mb-12'>
-            <ul className='flex flex-wrap gap-x-8 gap-y-3 items-center justify-center text-foreground/70 text-base lg:text-lg capitalize'>
-                <li className='link'>Все категории</li>
+            <ul className='flex flex-wrap gap-x-8 gap-y-3 items-center justify-center text-foreground/70 hover:text-foreground/70 text-base lg:text-lg capitalize'>
+                <li>
+                    <Link href={`/portfolio`} className='link'>Все категории</Link>
+                </li>
                 {
-                    pricing?.map((item) => <li key={item.title} className=''>{item.title}</li>)
+                    pricing?.map((item) => <li key={item.title} className=''>
+                        <Link href={`/`} className='hover:text-link transition-colors'>{item.title}</Link>
+                    </li>)
                 }
             </ul>
         </div>
