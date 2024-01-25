634d5652b3a77fc33fa6d7114eedaea36929a64d
k.dubov <example@example.com>
2024-01-25 16:10:29 +0300

    add projects filter

--- src/components/shared/Projects/Projects.tsx
+++ src/components/shared/Projects/Projects.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ProjectCard from './ProjectCard';
 import getProjects from '@/lib/getProjects';
+import ProjectsFilter from './ProjectsFilter';
 
 export default async function Projects() {
     const projects = await getProjects();
@@ -8,6 +9,7 @@ export default async function Projects() {
     return (
         <section className="section">
             <div className="container">
+                <ProjectsFilter/>
                 <ul className="grid grid-cols-[repeat(auto-fit,_minmax(min(400px,_100%),_1fr))] gap-8 gap-y-12 lg:gap-y-16">
                     {
                         projects.map(({ ...data }, index) => {
