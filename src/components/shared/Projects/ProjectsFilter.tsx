781ff508459d01dc79139d43e0e0d810c929c2be
k.dubov <example@example.com>
2024-01-30 12:55:18 +0300

    add badges to filter

--- src/components/shared/Projects/ProjectsFilter.tsx
+++ src/components/shared/Projects/ProjectsFilter.tsx
@@ -1,3 +1,4 @@
+import { badgeVariants } from '@/components/ui/badge'
 import { pricing } from '@/content'
 import Link from 'next/link'
 import React from 'react'
@@ -7,11 +8,11 @@ export default function ProjectsFilter() {
         <div className='flex flex-wrap items-center justify-center gap-4 mb-12'>
             <ul className='flex flex-wrap gap-x-8 gap-y-3 items-center justify-center text-foreground/70 hover:text-foreground/70 text-base lg:text-lg capitalize'>
                 <li>
-                    <Link href={`/portfolio`} className='link'>Все категории</Link>
+                    <Link href={`/portfolio`} className={badgeVariants({ variant: 'default' })}>Все категории</Link>
                 </li>
                 {
                     pricing?.map((item) => <li key={item.title} className=''>
-                        <Link href={`/`} className='hover:text-link transition-colors'>{item.title}</Link>
+                        <Link href={`/`} className={badgeVariants({ variant: 'secondary' })}>{item.title}</Link>
                     </li>)
                 }
             </ul>
