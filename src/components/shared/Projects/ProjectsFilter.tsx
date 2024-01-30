356533f33cae55be5a90879fe3d68b8df7276204
k.dubov <example@example.com>
2024-01-30 13:58:44 +0300

    update project filter

--- src/components/shared/Projects/ProjectsFilter.tsx
+++ src/components/shared/Projects/ProjectsFilter.tsx
@@ -1,19 +1,57 @@
+'use client'
 import { badgeVariants } from '@/components/ui/badge'
 import { pricing } from '@/content'
+import { cn } from '@/lib/utils'
 import Link from 'next/link'
 import React from 'react'
+import styles from './styles.module.css'
+import { usePathname } from 'next/navigation'
+import { cva } from 'class-variance-authority'
+
+const filterItemStyle = cva(
+    cn(badgeVariants({ variant: 'secondary' })),
+    {
+        variants: {
+            variant: {
+                primary: ""
+            },
+            state: {
+                active: [cn(badgeVariants({ variant: 'default' })), "text-base lg:text-lg"]
+            }
+        }
+    }
+)
 
 export default function ProjectsFilter() {
+    const pathname = usePathname();
+    const allFilters = pathname === '/portfolio';
+
     return (
-        <div className='flex flex-wrap items-center justify-center gap-4 mb-12'>
-            <ul className='flex flex-wrap gap-x-8 gap-y-3 items-center justify-center text-foreground/70 hover:text-foreground/70 text-base lg:text-lg capitalize'>
+        <div className={cn(styles['filter'])}>
+            <ul className={cn(styles['filter-list'])}>
                 <li>
-                    <Link href={`/portfolio`} className={badgeVariants({ variant: 'default' })}>Все категории</Link>
+                    <Link href={`/portfolio`} className={cn(badgeVariants({ variant: 'default' }),
+                        "leading-6",
+                        {
+                            "text-base lg:text-lg": allFilters
+                        }
+                    )}>Все категории</Link>
                 </li>
                 {
-                    pricing?.map((item) => <li key={item.title} className=''>
-                        <Link href={`/`} className={badgeVariants({ variant: 'secondary' })}>{item.title}</Link>
-                    </li>)
+                    pricing?.map((item) => {
+                        const activeFilter = pathname === item.link;
+
+                        return (
+                            <li key={item.title} className=''>
+                                <Link href={`${item.link}`} className={cn(badgeVariants({ variant: 'secondary' }),
+                                    "leading-6",
+                                    {
+                                        "text-base lg:text-lg": activeFilter
+                                    }
+                                )}>{item.title}</Link>
+                            </li>
+                        )
+                    })
                 }
             </ul>
         </div>
