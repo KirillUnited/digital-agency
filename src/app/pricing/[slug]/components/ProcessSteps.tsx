a4547887b8a04536fa1d0c249a14b7c29de5e8b8
k.dubov <example@example.com>
2024-02-22 18:07:18 +0300

    [service page] Add process steps

---
+++ src/app/pricing/[slug]/components/ProcessSteps.tsx
@@ -0,0 +1,30 @@
+'use client'
+import CTAModal from '@/components/shared/Modal/CTAModal'
+import StepsCard from '@/components/shared/Steps/StepsCard'
+import { Button } from '@/components/ui/button'
+import { Dialog, DialogTrigger } from '@/components/ui/dialog'
+import { ProjectType } from '@/lib/getProjects'
+import React from 'react'
+
+export default function ProcessSteps({ ...items }: ProjectType) {
+    return (
+        <section className='section bg-background-accent'>
+            <div className="container">
+                <div className={`flex flex-wrap items-center justify-between gap-4`}>
+                    <h2 className="heading-2">Этапы работ</h2>
+                </div>
+                <ul className="content grid grid-cols-[repeat(auto-fit,_minmax(270px,_1fr))] gap-6 py-8 lg:py-16">
+                    {
+                        items?.steps?.map(({ description }, index: number) => <li key={index}><StepsCard step={index + 1} title={description} /></li>)
+                    }
+                </ul>
+                <Dialog>
+                    <DialogTrigger asChild>
+                        <Button className='w-full sm:w-fit'>Заказать</Button>
+                    </DialogTrigger>
+                    <CTAModal />
+                </Dialog>
+            </div>
+        </section>
+    )
+}
