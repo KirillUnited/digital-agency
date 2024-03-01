446bc8523d6f6d40107e23dac6470efc372af4a6
k.dubov <example@example.com>
2024-03-01 16:17:33 +0300

    Update "Marketing" section

--- src/components/shared/Marketing/Marketing.tsx
+++ src/components/shared/Marketing/Marketing.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import MarketingCard from './MarketingCard'
 import { marketing } from '@/content'
 import { getPosts } from '@/lib/getProjects'
+import Link from 'next/link'
+import { MoveRightIcon } from 'lucide-react'
+import { Button } from '@/components/ui/button'
 
 const Marketing = async () => {
     const data = await getPosts('services');
@@ -9,8 +12,8 @@ const Marketing = async () => {
     return (
         <section className="section bg-accent">
             <div className="container">
-                <div className="lg:text-center flex flex-col items-center mb-8 lg:mb-12">
-                    <h2 className="heading-2 max-w-3xl">Реализуем проекты с надежными технологиями и эффективным маркетингом.</h2>
+                <div className="heading items-center mb-8 lg:mb-12 max-w-3xl">
+                    <h2 className="heading-2">Реализуем проекты с надежными технологиями и эффективным маркетингом.</h2>
                 </div>
                 <div className="marketing-grid gap-12">
                     {
@@ -23,6 +26,9 @@ const Marketing = async () => {
                             link={`/services/${item?.slug}`}
                         />)
                     }
+                    <Button asChild variant={'secondary'}>
+                        <Link href={'/services'} className='self-center'>Смотреть ещё</Link>
+                    </Button>
                 </div>
             </div>
         </section>
