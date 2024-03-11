cffb73d09eeea257d3aaf09f830de3e058b802ed
k.dubov <example@example.com>
2024-03-11 12:34:15 +0300

    refact

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import Hero from './components/Hero/Hero'
-import Mission from './components/Mission/Mission'
-import { site } from '@/content'
 import AboutUs from './components/AboutUs/AboutUs';
 import Benefits from './components/Benefits';
 import { ProjectType, getPost } from '@/lib/getProjects';
