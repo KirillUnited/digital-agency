6a6a33c45bc73b65228d332b1c8a414e35470855
k.dubov <example@example.com>
2024-03-11 19:25:40 +0300

    [features] fetch data from CMS

--- src/app/features/page.tsx
+++ src/app/features/page.tsx
@@ -1,28 +1,46 @@
 import React from 'react'
-import Hero from './components/Hero'
 import Benefits from '@/app/about/components/Benefits'
 import Faq from '@/components/shared/Faq/Faq'
-import { site } from '@/content'
-import MarketingCard from '@/components/shared/Marketing/MarketingCard'
+import MarketingCard, { MarketingCardProps } from '@/components/shared/Marketing/MarketingCard'
 import { cn } from '@/lib/utils'
 import styles from './styles.module.css'
+import { getPost } from '@/lib/getProjects'
+import Hero from '@/components/shared/Hero/Hero'
+import AboutUs from '../about/components/AboutUs/AboutUs'
 
 export async function generateMetadata() {
-    const title = site?.features?.hero?.pageTitle;
-    
+    const data = await getPost('features', 'about');
+
     return {
-        title
+        title: data?.title
     }
 }
 
-export default function FeaturesPage() {
+export default async function FeaturesPage() {
+    const { hero_sections, article_image_sections }: any = await getPost('features', 'about');
+    const marketing_cols = article_image_sections.filter((section: any) => section.type === 'marketing_cols');
+    const marketing_row = article_image_sections.filter((section: any) => section.type === 'marketing_row');
+
     return (
         <>
-            <Hero />
+            {hero_sections[0].type === "hero" &&
+                <Hero
+                    title={hero_sections[0].title}
+                    description={hero_sections[0].description}
+                    image={hero_sections[0].image}
+                    links={hero_sections[0].hero_links}
+                />
+            }
             <Benefits />
+            {marketing_cols.length > 0 && (
+                marketing_cols.map((section: any) => {
+                    return <AboutUs key={section.title} title={section.title} content={section.content} image={section.image} />
+                })
+            )
+            }
             <div className="marketing-grid">
                 {
-                    site.features?.content.map((item, index) => {
+                    marketing_row.map((item: MarketingCardProps) => {
                         return (
                             <section key={item.title} className={cn("section", styles.section)}>
                                 <MarketingCard {...item} className='container' />
